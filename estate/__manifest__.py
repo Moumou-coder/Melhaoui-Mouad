2554c99ebac602aeb45b79e6663389a40a14e162
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2022-12-07 11:27:01 +0100

    update manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,11 +2,12 @@
 {
     'name': "helb_project",
     'summary': """Project for HELB Student""",
-    'author': "My Company",
+    'author': "Mouad MELHAOUI",
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '16.0.0.1',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': [
-    ]
+    ],
+    'license': 'LGPL-3',
 }
