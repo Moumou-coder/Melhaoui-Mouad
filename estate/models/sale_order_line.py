c4fcf8c24c36124db0605c48d71e73a51616e790
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2022-12-21 16:18:32 +0100

    add employee field v2

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
-    training_employee = fields.Many2One(string="Employee")
+    training_employee = fields.Many2One("hr.employee", string="Employee")
