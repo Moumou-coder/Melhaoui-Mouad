6872776fefc3ca9903d6fe0f6fc30fd96d9bf573
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 20:12:29 +0100

    add employee model

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,6 +3,7 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
+    employee = fields.Many2one("hr.employee", string="employee")
 
     def action_confirm(self):
         res = super().action_confirm()
@@ -12,6 +13,7 @@ class SaleOrderLine(models.Model):
             'start': self.training_date,
             'stop': self.training_date,
             'allday': True,
+            'partner_id': self.employee.id,
         })
 
         return res
