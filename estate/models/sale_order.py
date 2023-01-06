d26b5a6055e66fcb8bb2b3c83d868663c0a70288
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 18:32:10 +0100

    add an event to calendar

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,3 +3,14 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    def action_confirm(self):
+
+        result = super().action_confirm()
+        event = self.env['calendar.event'].create({
+            'name': self.name,
+            'start': self.date_order,
+            'allday': True,
+        })
+        
+        return result
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,4 +3,3 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
-    employee = fields.Many2one("hr.employee", string="employee")
