5f54a088c1d88b08de45ef7fc9b6854c9b8bbc2f
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 19:54:25 +0100

    code that works for the event without training_date

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -8,16 +8,12 @@ class SaleOrder(models.Model):
 
         res = super().action_confirm()
 
-        sale_order_line = self.env['sale.order.line']
-        sale_order_line_field = sale_order_line.read(['training_date'])
-        sale_order_line_date = sale_order_line_field['training_date']
-
         event = self.env['calendar.event'].create({
-            'name': self.name,
-            'start': sale_order_line_date,
-            #'stop': self.training_date,
-            'allday': True,
             'partner_id': self.partner_id.id,
+            'name': self.name,
+            'start': self.date_order,
+            'stop': self.date_order,
+            'allday': True,
         })
         
         return res
