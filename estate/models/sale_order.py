c59f8154991494614a9fedef1b22d496d53ef466
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 19:50:19 +0100

    test retrieve training_date v2

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,19 +3,21 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    sale_order_line = self.env['sale.order.line']
     
     def action_confirm(self):
 
         res = super().action_confirm()
-        sale_order_line_date = sale_order_line.training_date
+
+        sale_order_line = self.env['sale.order.line']
+        sale_order_line_field = sale_order_line.read(['training_date'])
+        sale_order_line_date = sale_order_line_field['training_date']
 
         event = self.env['calendar.event'].create({
-            'partner_id': self.partner_id,
             'name': self.name,
             'start': sale_order_line_date,
             #'stop': self.training_date,
             'allday': True,
+            'partner_id': self.partner_id.id,
         })
         
         return res
