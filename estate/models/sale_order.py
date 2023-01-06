7f2a66fb151b8b65090e64aa895e2b7d1473d2be
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 22:46:32 +0100

    employee v10

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     training_date = fields.Date(string="Training Date")
-    employee = fields.Many2one("hr.employee", string="employee")
+    #employee = fields.Many2one("hr.employee", string="employee")
 
     
     
@@ -14,18 +14,20 @@ class SaleOrder(models.Model):
 
         order_line_id = self.order_line.id
         order_line = self.env['sale.order.line'].browse(order_line_id)
-        #employee = order_line.employee
-        #employee = self.env['hr.employee'].browse(employee)
-        employee_id = order_line.user_id.id
         training_date = order_line.training_date
         description = order_line.name
+
+        #employee = order_line.employee
+        #employee = self.env['hr.employee'].browse(employee)
+        #employee_id = order_line.user_id.id
+        
         
         event = self.env['calendar.event'].create({
             'name': description,
             'start': training_date,
             'stop': training_date,
             'allday': True,
-            'partner_id': partner_id,
+            'partner_id': partner_id.id,
         })
         
         return res
