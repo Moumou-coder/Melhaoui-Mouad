fe8dc7c14e3f367513c88feab4e5c6289ea667db
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 20:55:16 +0100

    training_date avec self env

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,13 +3,14 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    training_date = fields.Date(string="Training Date")
-    employee = fields.Many2one("hr.employee", string="employee")
-    #description = fields.Text(string='Description')
+    
     
     def action_confirm(self):
         
         res = super().action_confirm()
+
+        order_line = self.env['sale.order.line'].browse(order_line_id)
+        training_date = order_line.training_date
         
         event = self.env['calendar.event'].create({
             'name': self.name,
@@ -17,7 +18,6 @@ class SaleOrder(models.Model):
             'stop': self.training_date,
             'allday': True,
             'partner_id': self.partner_id.id,
-            'employee': self.employee.id
         })
         
         return res
