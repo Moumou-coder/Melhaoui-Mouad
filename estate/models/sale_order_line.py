9f1f9811db5e9b8bbd29efa595655567237d64d7
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-06 22:44:04 +0100

    employee v9

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,16 +4,3 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
     employee = fields.Many2one("hr.employee", string="employee")
-
-    def action_confirm(self):
-        res = super().action_confirm()
-
-        event = self.env['calendar.event'].create({
-            'name': self.name,
-            'start': self.training_date,
-            'stop': self.training_date,
-            'allday': True,
-            'partner_id': self.employee.id,
-        })
-
-        return res
