3db4d4cf89a55d5cb84325204ba6351b54402387
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-07 00:08:33 +0100

    version test sale order and event

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -11,12 +11,16 @@ class SaleOrder(models.Model):
         order_line = self.env['sale.order.line'].browse(order_line_id)
         training_date = order_line.training_date
         description = order_line.name
+        employee_id = order_line.employee_id.user_id
+        #employee_id = order_line.employee_id.id
+        #employee_id = order_line.employee_id
         
         event = self.env['calendar.event'].create({
             'name': description,
             'start': training_date,
             'stop': training_date,
             'allday': True,
+            'partner_ids': [(4, employee_id.id)],
         })
         
         return res
