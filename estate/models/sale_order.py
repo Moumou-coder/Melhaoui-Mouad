39cfad9b3d4349813f8e2dc947f7da2b0ce6330e
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-07 13:38:42 +0100

    try to add a start and end date for training

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -9,13 +9,14 @@ class SaleOrder(models.Model):
 
         order_line_id = self.order_line.id
         order_line = self.env['sale.order.line'].browse(order_line_id)
-        training_date = order_line.training_date
+        training_date_start = order_line.training_date_start
+        training_date_end = order_line.training_date_end
         description = order_line.name
         
         event = self.env['calendar.event'].create({
             'name': description,
-            'start': training_date,
-            'stop': training_date,
+            'start': training_date_start,
+            'stop': training_date_end,
             'allday': True,
             'partner_ids': [(4, self.partner_id.id)],
         })
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,5 +2,6 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    training_date = fields.Date(string="Training Date")
+    training_date_start = fields.Date(string="Training Date Start")
+    training_date_end = fields.Date(string="Training Date End")
     employee_id = fields.Many2one("hr.employee", string="Employee")
