ad2fef8cc3b178d7876324298399c22783b479f6
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-07 23:22:12 +0100

    limit amount db

--- estate/models/res_partner.py
+++ estate/models/res_partner.py
@@ -2,5 +2,5 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    limit_amount_sale_order = fields.Monetary(string='Limit Amount Training', currency_field='currency_id', digits=(12, 2))
+    limit_amount_sale_order = fields.Monetary(string='Limit Amount Training')
 
