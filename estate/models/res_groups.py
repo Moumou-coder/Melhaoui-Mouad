9594f9f64e3979239b79246b05d57c4a6178f619
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-01-07 16:34:04 +0100

    version that works to display max_amount

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -2,4 +2,4 @@ from odoo import fields, models
 
 class ResGroups(models.Model):
     _inherit = 'res.groups'
-    max_amount = fields.Float(string = "training max price (Approval)")
+    max_amount = fields.Float(string = "training max price")
